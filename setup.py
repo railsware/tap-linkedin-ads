b1db6b3f3bcc9b665b8f02e0365559c8c7a2023d
Andrii Porokhnavets <andrii.porokhnavets@railsware.com>
2025-05-16 12:09:32 +0300

    [CPL-20326] Bump singer-python version to align with Airbyte CDK

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='tap-linkedin-ads',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_linkedin_ads'],
       install_requires=[
-          'singer-python@git+https://github.com/railsware/singer-python/@ea0489dcb73fcb19195eea50eeff9a13d6914e9a',
+          'singer-python@git+https://github.com/railsware/singer-python/@565fcb685e6a636c3ad21e421a0662da47757573',
           'requests>=2.31.0',
           'backoff~=2.2.1',
       ],
